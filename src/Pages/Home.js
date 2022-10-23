851228f811a71b9f84104f492f27b0c25926d7e0
izaanjahangir <izaanjahangir2@gmail.com>
2022-10-23 22:52:56 +0500

    working on post template

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,18 +4,52 @@ import Post from "../components/Post";
 
 export default function Home() {
   let posts = [
-    { title: "Iphone", desc: "yghhfjhtfjthdgjdcjgh" },
-    { title: "Android", desc: "yghhfjhtfjthdgjdcjgh" },
-    { title: "Amazon", desc: "yghhfjhtfjthdgjdcjgh" },
+    {
+      title: "Iphone",
+      desc: "yghhfjhtfjthdgjdcjgh",
+      image:
+        "https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?cs=srgb&dl=pexels-pixabay-371589.jpg&fm=jpg",
+      address: "Malir",
+    },
+    {
+      title: "Android",
+      desc: "yghhfjhtfjthdgjdcjgh",
+      image:
+        "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?cs=srgb&dl=pexels-pixabay-206359.jpg&fm=jpg",
+      address: "North karachi",
+    },
+    {
+      title: "Amazon",
+      desc: "yghhfjhtfjthdgjdcjgh",
+      image:
+        "https://img.freepik.com/free-vector/flat-design-lake-scenery_23-2149161405.jpg?w=2000",
+      address: "Defence",
+    },
+    {
+      title: "Amazon",
+      desc: "yghhfjhtfjthdgjdcjgh",
+      image:
+        "https://img.freepik.com/free-vector/flat-design-lake-scenery_23-2149161405.jpg?w=2000",
+      address: "Defence",
+    },
+    {
+      title: "Amazon",
+      desc: "yghhfjhtfjthdgjdcjgh",
+      image:
+        "https://img.freepik.com/free-vector/flat-design-lake-scenery_23-2149161405.jpg?w=2000",
+      address: "Defence",
+    },
   ];
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-        {posts.map((post, idx) => (
-          <Post key={idx} data={post} />
-        ))}
+        <div className="row">
+          {posts.map((post, idx) => (
+            <Post key={idx} data={post} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function Post({data}) {
+export default function Post({ data }) {
   return (
-    <div>
-        {data.title}
-        <br/>
-        {data.desc}
+    <div className="col-4">
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={data.image}
+          alt="Card image"
+          style={{ width: "100%" }}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{data.title}</h4>
+          <p className="card-text">{data.desc}</p>
+          <a href="#" className="btn btn-primary">
+            See Profile
+          </a>
+
+          <p style={{ textAlign: "left" }} className="card-text">
+            {data.address}
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
