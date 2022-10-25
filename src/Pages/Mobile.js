eab10727913bb7c13f1e7b70665e322d266f832d
LAIBA SHAKEEL <83995569+laiba-shakeel@users.noreply.github.com>
2022-10-25 21:35:22 +0500

    initializing the code

--- src/Pages/Mobile.js
+++ src/Pages/Mobile.js
@@ -1,12 +1,122 @@
-import React from 'react'
-import Slider from 'react-slick'
-import Navbar from '../components/Navbar'
+import { Typography } from "@mui/material";
+import React from "react";
+import Slider from "../components/Slider";
+import Navbar from "../components/Navbar";
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+const options = [
+  'None',
+  'Atria',
+  'Callisto',
+  'Dione',
+  'Ganymede',
+  'Hangouts Call',
+  'Luna',
+  'Oberon',
+  'Phobos',
+  'Pyxis',
+  'Sedna',
+  'Titania',
+  'Triton',
+  'Umbriel',
+];
+
+const ITEM_HEIGHT = 48;
 
 export default function Mobile() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
-    <Navbar/>
-     <Slider/>
+      <Navbar />
+      <div className="container">
+        <div className="img">
+          <img
+            src="https://tpc.googlesyndication.com/simgad/11727365248570226062?sqp=4sqPyQQ7QjkqNxABHQAAtEIgASgBMAk4A0DwkwlYAWBfcAKAAQGIAQGdAQAAgD-oAQGwAYCt4gS4AV_FAS2ynT4&rs=AOga4qkG757jb5bJXnofBsOQm7sOT1jyAg"
+            alt=""
+            width="100%"
+            height="70%"
+          />
+        </div>
+        <div className="subHead container p-3">
+          <Typography
+            variant="p"
+            sx={{
+              fontSize: "18px",
+              fontFamily: "sans-serif",
+              display: "flex",
+              cursor: "pointer",
+            }}>
+            <p className="fw-bold p-2">Popular-Search</p>
+            <p className="p-2">Iphone</p>
+            <p className="p-2">Infinix</p>
+            <p className="p-2">Oppo</p>
+            <p className="p-2">Samsung</p>
+            <p className="p-2">Vivo</p>
+          </Typography>
+        </div>
+        <Slider />
+        <Typography
+          sx={{
+            display: "flex",
+            flexFlow: "right",
+            marginTop: "6%",
+          }}>
+          <h4>Mobile-Phones</h4>
+        </Typography>
+        <Typography >
+          <div className="row">
+            <div className="col-lg-3 d-flex p-3 fw-bold"><p>Filters</p> 
+            <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
     </div>
-  )
+            </div>
+            <div className="col-lg-2">
+              
+            </div>
+            <div className="col-lg-7">item3</div>
+            <div></div>
+          </div>
+        </Typography>
+      </div>
+    </div>
+  );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,41 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
-
-
 export default class SimpleSlider extends Component {
+
   render() {
     const settings = {
-      dots: true,
+      dots: false,
       infinite: true,
-      speed: 500,
+      speed: 300,
       slidesToShow: 1,
       slidesToScroll: 1
     };
+
     return (
-      <div>a
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+      <div sx={{ justifyContent: "center"}}>
+        <h2>Core i7 Genereration</h2>
+        <Slider {...settings} m={0}>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" width="100%" height="100%" />
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""  width="100%" height="100%"/>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1661961111247-e218f67d1cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" width="100%" height="100%" />
           </div>
-          <div>
-            <h3>4</h3>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="100%" height="100%" />
           </div>
-          <div>
-            <h3>5</h3>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt=""  width="100%" height="100%"/>
           </div>
-          <div>
-            <h3>6</h3>
+          <div className="mainSlider container">
+           <img src="https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="100%" height="100%"/>
           </div>
         </Slider>
       </div>
     );
-    
   }
 }
