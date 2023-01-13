e340aa24c60e8935bb45fce6ddc56c8c56630893
LAIBA SHAKEEL <83995569+laiba-shakeel@users.noreply.github.com>
2023-01-13 21:26:05 +0500

    updated comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import Navigation from "./Navigation";
 
--- src/components/TagGetApps.js
+++ src/components/TagGetApps.js
@@ -1,44 +1,53 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import img1 from "../assets/olxMobile.jpg"; 
+import React from "react";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import {
+  Container,
+  Row,
+  Col,
+}from "react-bootstrap"
+import img1 from "../assets/olxMobile.jpg";
 
 const TagGetApps = () => {
   return (
     <div>
-          <Container className='mb-5'>
-      <Row className='justify-content-md-center'>
-        <Col>
-        <div>
-<img src={img1} alt='img'/>
-</div>
-        </Col>
-      <Col>
-      <div className='centerLine' 
-      style={{
-
-        width: "0.2rem",
-    margin: "0 auto",
-    backgroundColor: "rgba(0,47,52,.2)",
-    height: "10.5rem"
+      <Container fluid className="mb-5 border" style={{
+        BackgroundColor: "#808080"
       }}>
-        </div>
-        </Col>
-        <Col className='ms-0 , me-0'><div className='LastOne'>
-            <h3 style={{
-               color: "Black",
-               fontSize: "18px",
-               fontWeight: "bolder",
-
-            }}>Get your App Today</h3>
-            
-        </div>
-        </Col>
-      </Row>
+        <Row className="">
+          <Col xs={6}>
+            <div>
+              <img src={img1} alt="img" />
+            </div>
+          </Col>
+          <Col>
+            <div
+              className="align-items-center"
+              style={{
+                width: "0.2rem",
+                margin: "0 auto",
+                backgroundColor: "rgba(0,47,52,.2)",
+                height: "10.5rem",
+                textAlign: "center"
+              }}></div>
+          </Col>
+          <Col className="ms-0 , me-0">
+            <div className="LastOne">
+              <h3
+                style={{
+                  color: "Black",
+                  fontSize: "18px",
+                  fontWeight: "bolder",
+                }}>
+                Get your App Today
+              </h3>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default TagGetApps
+export default TagGetApps;
